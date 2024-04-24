a20c6676efacc9bad5c5a5c28c11b5d6a96b6885
HyperWinX <artmsmth@gmail.com>
2024-04-24 20:46:56 +0400

    Fixed several unsafe things, improved memory usage

--- leakhelper.c
+++ leakhelper.c
@@ -2,7 +2,6 @@
 
 typedef struct _ptr_t{
     void* ptr;
-    int valid;
 } ptr_t;
 
 static ptr_t* listing;
@@ -17,37 +16,32 @@ int init_listing(int ptrcount){
 
 void* lcalloc(int size){
     void* ptr = calloc(size, 1);
-    for (int i = 0; i < listing_ptrcount; i++){
-        if (!listing[i].valid){
-            listing[i].ptr = ptr;
-            listing[i].valid = 1;
-            break;
-        }
-    }
+    int free_element = 0;
+    while (free_element < listing_ptrcount && !listing[free_element].ptr);
+    listing[free_element].ptr = ptr;
     return ptr;
 }
 
 void* lmalloc(int size){
     void* ptr = malloc(size);
-    for (int i = 0; i < listing_ptrcount; i++){
-        if (!listing[i].valid){
-            listing[i].ptr = ptr;
-            listing[i].valid = 1;
-            break;
-        }
-    }
+    int free_element = 0;
+    while (free_element < listing_ptrcount && listing[free_element].ptr) free_element++;
+    listing[free_element].ptr = ptr;
     return ptr;
 }
 
 void lfree(void* ptr){
     int offset = 0;
-    while (listing[offset].ptr != ptr) offset++;
-    listing[offset].valid = 0;
+    while (listing[offset].ptr != ptr && offset < listing_ptrcount) offset++;
+    listing[offset].ptr = 0;
     free(ptr);
 }
 
 void free_all(){
     for (int i = 0; i < listing_ptrcount; i++)
-        if (listing[i].valid) free(listing[i].ptr);
+        if (listing[i].ptr){
+            free(listing[i].ptr);
+            listing[i].ptr = 0;
+        }
     free(listing);
 }
